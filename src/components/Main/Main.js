ae721abe3df727b08d449a2a8a5bffa6cbe5b1ff
srvds8080 <srvds@yandex.ru>
2021-02-17 00:50:32 +0300

    feat: Добавить компонент Main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+import Header from '../Header/Header';
+import Promo from '../Promo/Promo';
+
+const Main = () => (
+  <div className="main">
+    <Header />
+    <Promo />
+  </div>
+);
+
+export default Main;
