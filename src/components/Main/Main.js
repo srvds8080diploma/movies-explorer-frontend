e5496b0afb0bf13011e79348b77ab7f0825c9600
srvds8080 <srvds@yandex.ru>
2021-02-17 22:40:55 +0300

    feat: Отрендерить в главной странице секцию о проекте

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './Main.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
+import ProjectSection from '../ProjectSection/ProjectSection';
+import AboutProject from '../AboutProject/AboutProject';
 
 const Main = () => (
   <div className="main">
     <Header />
     <Promo />
+    <ProjectSection id="About" title="О проекте">
+      <AboutProject />
+    </ProjectSection>
   </div>
 );
 
