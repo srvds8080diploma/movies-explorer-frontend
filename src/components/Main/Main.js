c8b870d17a1538e1c52dede70554d7fa4740dc05
srvds8080 <srvds@yandex.ru>
2021-02-18 00:23:06 +0300

    feat: Добавить компонент технологий

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,7 @@ import Promo from '../Promo/Promo';
 import ProjectSection from '../ProjectSection/ProjectSection';
 import AboutProject from '../AboutProject/AboutProject';
 import Progressbar from '../Progressbar/Progressbar';
+import Stack from '../Stack/Stack';
 
 const Main = () => (
   <div className="main">
@@ -13,6 +14,9 @@ const Main = () => (
     <ProjectSection id="About" title="О проекте">
       <AboutProject />
       <Progressbar />
+    </ProjectSection>
+    <ProjectSection typeBackground id="stack" title="Технологии">
+      <Stack />
     </ProjectSection>
   </div>
 );
---
+++ src/components/Stack/Stack.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Stack.css';
+
+const Stack = () => (
+  <div className="Stack">
+    <h4 className="Stack__title">7 технологий</h4>
+    <p className="Stack__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+  </div>
+);
+
+export default Stack;
