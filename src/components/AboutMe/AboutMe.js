f41c2cd41ae8564c6d2dd80ecb55c671534e9e54
srvds8080 <srvds@yandex.ru>
2021-02-21 16:45:26 +0300

    feat: Добавить компонент aboutme

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,18 +4,20 @@ import me from '../../images/me.png';
 
 const AboutMe = () => (
   <div className="about-me">
-    <h3 className="about-me__title">Виталий</h3>
-    <p className="about-me__subtitle">Фронтенд-разработчик, 30 лет</p>
-    <p className="about-me__description">
-      { 'Я родился и живу в Саратове, закончил факультет экономики СГУ.'
-      + ' У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.'
-      + ' Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».'
-      + ' После того, как прошёл курс по веб-разработке, начал заниматься'
-      + ' фриланс-заказами и ушёл с постоянной работы.'}
-    </p>
-    <div className="about-me__links">
-      <a href="https://github.com/srvds8080" rel="noreferrer" target="_blank" alt="facebook" className="about-me__links_item">Facebook</a>
-      <a href="https://github.com/srvds8080" rel="noreferrer" target="_blank" alt="facebook" className="about-me__link-item">Github</a>
+    <div className="about-me__wrapper">
+      <h3 className="about-me__title">Виталий</h3>
+      <p className="about-me__subtitle">Фронтенд-разработчик, 30 лет</p>
+      <p className="about-me__description">
+        { 'Я родился и живу в Саратове, закончил факультет экономики СГУ.'
+        + ' У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.'
+        + ' Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».'
+        + ' После того, как прошёл курс по веб-разработке, начал заниматься'
+        + ' фриланс-заказами и ушёл с постоянной работы.'}
+      </p>
+      <div className="about-me__links">
+        <a href="https://github.com/srvds8080" rel="noreferrer" target="_blank" alt="facebook" className="about-me__links-item">Facebook</a>
+        <a href="https://github.com/srvds8080" rel="noreferrer" target="_blank" alt="Github" className="about-me__links-item">Github</a>
+      </div>
     </div>
     <img src={me} alt={me} className="about-me__image" />
   </div>
