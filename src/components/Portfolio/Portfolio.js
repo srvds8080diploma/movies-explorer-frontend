308b85199ec7f355890f3ea8dea88b23ce620024
srvds8080 <srvds@yandex.ru>
2021-02-23 01:44:21 +0300

    feat: Добавить компонент Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import './Portfolio.css';
-import me from '../../images/me.png';
 
 const Portfolio = () => (
-  <div className="about-me">
-    <div className="about-me__wrapper">
-      <h3 className="about-me__title">Виталий</h3>
-      <p className="about-me__subtitle">Фронтенд-разработчик, 30 лет</p>
-      <p className="about-me__description">
-        { 'Я родился и живу в Саратове, закончил факультет экономики СГУ.'
-        + ' У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.'
-        + ' Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».'
-        + ' После того, как прошёл курс по веб-разработке, начал заниматься'
-        + ' фриланс-заказами и ушёл с постоянной работы.'}
-      </p>
-      <div className="about-me__links">
-        <a href="https://github.com/srvds8080" rel="noreferrer" target="_blank" alt="facebook" className="about-me__links-item">Facebook</a>
-        <a href="https://github.com/srvds8080" rel="noreferrer" target="_blank" alt="Github" className="about-me__links-item">Github</a>
-      </div>
-    </div>
-    <img src={me} alt={me} className="about-me__image" />
+  <div className="portfolio">
+    <h5 className="portfolio__title">Портфолио</h5>
+    <ul className="portfolio__list">
+      <li className="portfolio__list-item">
+        <a className="portfolio__link" target="_blanc" href="https://github.com/srvds8080">Статичный сайт</a>
+      </li>
+      <li className="portfolio__list-item">
+        <a className="portfolio__link" target="_blanc" href="https://github.com/srvds8080">Адаптивный сайт</a>
+      </li>
+      <li className="portfolio__list-item">
+        <a className="portfolio__link" target="_blanc" href="https://github.com/srvds8080">Одностраничное приложение</a>
+      </li>
+    </ul>
   </div>
 );
 
