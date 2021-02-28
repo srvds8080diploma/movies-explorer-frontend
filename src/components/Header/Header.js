1992d756051cb322fa91d15cb4525d1c05d20366
srvds8080 <srvds@yandex.ru>
2021-03-01 01:46:28 +0300

    feat: Подготовить компонент Header для заполнения

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './Header.css';
-import { ReactComponent as Logo } from '../../images/logo.svg';
+import MenuProfile from '../MenuProfile/MenuProfile';
+import Navigation from '../Navigation/Navigation';
 
-const Header = ({ valueButton, valueTitle }) => (
+const Header = () => (
   <header className="header">
     <div className="header__content">
-      <Logo id="1" className="header__logo" />
-      <div className="header__profile">
-        <button type="button" className="header__title">{valueTitle}</button>
-        <button type="button" className="header__button">{valueButton}</button>
-      </div>
+      <button type="button" className="header__logo" />
+      {true && <Navigation /> }
+      {false && <MenuProfile />}
     </div>
   </header>
 );
 
-Header.defaultProps = {
-  valueButton: 'Войти',
-  valueTitle: 'Регистрация',
-};
-Header.propTypes = {
-  valueButton: PropTypes.string,
-  valueTitle: PropTypes.string,
-};
+// Header.defaultProps = {
+//   valueButton: 'Войти',
+//   valueTitle: 'Регистрация',
+//   isLogged: true,
+// };
+// Header.propTypes = {
+//   isLogged: PropTypes.bool,
+//   valueButton: PropTypes.string,
+//   valueTitle: PropTypes.string,
+// };
 
 export default Header;
