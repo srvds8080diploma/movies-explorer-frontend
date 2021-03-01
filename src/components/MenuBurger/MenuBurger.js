855ea90d9fcf2764e74a4fefd1e1999a30cc9de1
srvds8080 <srvds@yandex.ru>
2021-03-01 22:40:21 +0300

    feat: Меню-слайдер завершить

--- src/components/MenuBurger/MenuBurger.js
+++ src/components/MenuBurger/MenuBurger.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './MenuBurger.css';
+import Account from '../Account/Account';
 
 const MenuBurger = ({ classMenuBurgerValue }) => (
   <nav className={`menu-burger ${classMenuBurgerValue && 'menu-burger_active'}`}>
-    <ul className="menu-burger__list">
-      <li className="menu-burger__list-item">
-        <a href="https://google.com" className="menu-burger__list-link">Главная</a>
-      </li>
-      <li className="menu-burger__list-item">
-        <a href="https://google.com" className="menu-burger__list-link">Фильмы</a>
-      </li>
-      <li className="menu-burger__list-item">
-        <a href="https://google.com" className="menu-burger__list-link">Сохраненные фильмы</a>
-      </li>
-    </ul>
+    <div className={`menu-burger__container ${classMenuBurgerValue && 'menu-burger__container_active'}`}>
+      <ul className="menu-burger__list">
+        <li className="menu-burger__list-item">
+          <a href="https://google.com" className="menu-burger__list-link">Главная</a>
+        </li>
+        <li className="menu-burger__list-item">
+          <a href="https://google.com" className="menu-burger__list-link">Фильмы</a>
+        </li>
+        <li className="menu-burger__list-item">
+          <a href="https://google.com" className="menu-burger__list-link">Сохраненные фильмы</a>
+        </li>
+        <li className="menu-burger__list-item">
+          <Account />
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
