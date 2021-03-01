a8827846fe89821f66d3538b81b127026d9cbe2f
srvds8080 <srvds@yandex.ru>
2021-03-01 22:31:43 +0300

    feat: Создать компонент Account

---
+++ src/components/Account/Account.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Account.css';
+
+const Account = () => (
+  <div className="account">
+    <a href="https://github.com" className="account__link">
+      Аккаунт
+    </a>
+    <button type="button" className="account__image" />
+  </div>
+);
+
+export default Account;
