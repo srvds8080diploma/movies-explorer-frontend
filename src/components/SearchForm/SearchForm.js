4641d0dda8c9fcb79c3c3f71e66272632ad97ea7
srvds8080 <srvds@yandex.ru>
2021-03-05 22:09:30 +0300

    feat: Добавить компонент SearchForm

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ToggleCheckbox from '../ToggleCheckbox/ToggleCheckbox';
+import './SearchForm.css';
+
+const SearchForm = () => (
+  <form noValidate className="search-form">
+    <input
+      type="search"
+      required
+      placeholder="Фильм"
+      maxLength="100"
+      className="search-form__input"
+    />
+    <button type="submit" className="search-form__button">Найти</button>
+    <ToggleCheckbox />
+  </form>
+);
+
+export default SearchForm;
