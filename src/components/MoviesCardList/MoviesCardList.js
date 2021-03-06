b40387f6a4da55d1da3476289118bb630f3bbdbe
srvds8080 <srvds@yandex.ru>
2021-03-06 16:48:44 +0300

    feat: Добавить сетку для Movies

---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import MoviesCard from '../MoviesCard/MoviesCard';
+import './MoviesCardList.css';
+
+const MoviesCardList = () => (
+  <div className="movies-list">
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+    <MoviesCard />
+  </div>
+);
+
+export default MoviesCardList;
