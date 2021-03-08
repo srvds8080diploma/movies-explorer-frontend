f2f1c548d5c4e2be95fa1afc67fe5b30030866e5
srvds8080 <srvds@yandex.ru>
2021-03-09 02:00:15 +0300

    feat: Сдулать кликабельными ссылки

--- src/components/MenuProfile/MenuProfile.js
+++ src/components/MenuProfile/MenuProfile.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './MenuProfile.css';
 
 const MenuProfile = ({ valueButton, valueTitle }) => (
   <div className="menu-profile">
-    <button type="button" className="menu-profile__title">{valueTitle}</button>
-    <button type="button" className="menu-profile__button">{valueButton}</button>
+    <Link to="/signup">
+      <button type="button" className="menu-profile__title">{valueTitle}</button>
+    </Link>
+    <Link to="/signin">
+      <button type="button" className="menu-profile__button">{valueButton}</button>
+    </Link>
   </div>
 );
 
