23e05f75ddde5426e8ce9bb8c0833c5e281aab5f
srvds8080 <srvds@yandex.ru>
2021-03-08 23:56:42 +0300

    format Форматирование кода

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,16 +7,44 @@ const Profile = ({ name, titleSubmit, titleLogoutButton }) => (
   <div className="profile">
     <h4 className="profile__title">{`Привет, ${name}`}</h4>
     <form noValidate className="profile__form">
-      <label htmlFor="name" className="profile__input-label">
+      <label
+        htmlFor="name"
+        className="profile__input-label"
+      >
         Имя
-        <input id="name" maxLength="50" type="text" className="profile__input profile__input_type_name" placeholder="Виталий" />
+        <input
+          name="name"
+          maxLength="50"
+          type="text"
+          className="profile__input profile__input_type_name"
+          placeholder="Виталий"
+        />
       </label>
-      <label htmlFor="name" className="profile__input-label">
+      <label
+        htmlFor="name"
+        className="profile__input-label"
+      >
         Email
-        <input id="email" maxLength="50" type="email  " className="profile__input profile__input_type_email" placeholder="example@email.com" />
+        <input
+          name="email"
+          maxLength="50"
+          type="email  "
+          className="profile__input profile__input_type_email"
+          placeholder="example@email.com"
+        />
       </label>
-      <button type="submit" className="profile__button profile__button_type_edit">{ titleSubmit }</button>
-      <Link to="/sign-in" className="profile__button profile__button_type_logout">{ titleLogoutButton }</Link>
+      <button
+        type="submit"
+        className="profile__button profile__button_type_edit"
+      >
+        {titleSubmit}
+      </button>
+      <Link
+        to="/signup"
+        className="profile__button profile__button_type_logout"
+      >
+        {titleLogoutButton}
+      </Link>
     </form>
   </div>
 );
