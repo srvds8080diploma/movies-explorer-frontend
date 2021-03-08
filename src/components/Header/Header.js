11f5dcae4871e6fcdb673365499ea0ae282c96f6
srvds8080 <srvds@yandex.ru>
2021-03-08 23:57:22 +0300

    fix: Обобрадение хедера на разных страницах

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Header.css';
+import Logo from '../Logo/Logo';
 
-const Header = ({ children }) => (
-  <header className="header">
-    <div className="header__content">
-      <Link to="/" className="header__logo" />
-      {children}
-    </div>
-  </header>
-);
+const Header = ({ children, location }) => {
+  const [invisible, setInvisible] = useState(false);
+  useEffect(() => {
+    if (location.pathname === '/signup'
+      || location.pathname === '/signin') {
+      setInvisible(true);
+      return;
+    }
+    setInvisible(false);
+  }, [location]);
+  return (
+    <header className={`header ${invisible && 'header__invisible'}`}>
+      <div className="header__content">
+        <Logo path="/" />
+        {children}
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   children: PropTypes.node.isRequired,
+  location: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default Header;
