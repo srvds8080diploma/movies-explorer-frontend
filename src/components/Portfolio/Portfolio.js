09f103a4c1470b05a8fa226b51aa3f1257445288
srvds8080 <srvds@yandex.ru>
2021-03-09 03:55:08 +0300

    fix; Добавить якорь для ссылки

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,7 +3,7 @@ import './Portfolio.css';
 
 const Portfolio = () => (
   <div className="portfolio">
-    <h5 className="portfolio__title">Портфолио</h5>
+    <h5 id="portfolio" className="portfolio__title">Портфолио</h5>
     <ul className="portfolio__list">
       <li className="portfolio__list-item">
         <a className="portfolio__link" target="_blanc" href="https://github.com/srvds8080">Статичный сайт</a>
