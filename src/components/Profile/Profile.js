640938b193c5cc6d50aecf09ef5bc0ebece9e47d
srvds8080 <srvds@yandex.ru>
2021-03-13 19:35:59 +0300

    feat: Настроить авторизацию и валидацию форм profile и авторизации

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,62 +1,119 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Profile.css';
 
-const Profile = ({ name, titleSubmit, titleLogoutButton }) => (
-  <div className="profile">
-    <h4 className="profile__title">{`Привет, ${name}`}</h4>
-    <form noValidate className="profile__form">
-      <label
-        htmlFor="name"
-        className="profile__input-label"
-      >
-        Имя
-        <input
-          name="name"
-          maxLength="50"
-          type="text"
-          className="profile__input profile__input_type_name"
-          placeholder="Виталий"
-        />
-      </label>
-      <label
-        htmlFor="name"
-        className="profile__input-label"
-      >
-        Email
-        <input
-          name="email"
-          maxLength="50"
-          type="email  "
-          className="profile__input profile__input_type_email"
-          placeholder="example@email.com"
-        />
-      </label>
-      <button
-        type="submit"
-        className="profile__button profile__button_type_edit"
-      >
-        {titleSubmit}
-      </button>
-      <Link
-        to="/signup"
-        className="profile__button profile__button_type_logout"
-      >
-        {titleLogoutButton}
-      </Link>
-    </form>
-  </div>
-);
+const Profile = ({
+  titleSubmit, titleLogoutButton, onSubmit, onLogout,
+}) => {
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const [{
+    email, emailValid, name, nameValid, errors,
+  }, setFormValues] = useState({
+    email: '',
+    emailValid: false,
+    name: '',
+    nameValid: false,
+    errors: {
+      name: '',
+      email: '',
+    },
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      onSubmit({
+        email, name,
+      });
+    }
+  };
+
+  const handleOnChange = (e) => {
+    switch (e.target.name) {
+      case e.target.name:
+        setFormValues((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value,
+          [`${e.target.name}Valid`]: e.target.validity.valid,
+        }));
+        errors[e.target.name] = e.target.validationMessage;
+        break;
+      default:
+        break;
+    }
+    setIsFormValid(Array.from(e.target.form).every((element) => element.validity.valid));
+  };
+  return (
+    <div className="profile">
+      <h4 className="profile__title">Привет, Виталий!</h4>
+      <form onSubmit={handleSubmit} noValidate className="profile__form">
+        <label
+          htmlFor="name"
+          className="profile__input-label"
+        >
+          Имя
+          <input
+            name="name"
+            maxLength="30"
+            minLength="2"
+            type="text"
+            className="profile__input profile__input_type_name"
+            placeholder="Виталий"
+            value={name}
+            onChange={handleOnChange}
+          />
+          <span
+            className={`register__input-error ${nameValid && 'register__input-error_active'}`}
+          >
+            {errors.name}
+          </span>
+        </label>
+        <label
+          htmlFor="email"
+          className="profile__input-label"
+        >
+          Email
+          <input
+            name="email"
+            type="email"
+            className="profile__input profile__input_type_name"
+            placeholder="Виталий"
+            value={email}
+            onChange={handleOnChange}
+          />
+          <span
+            className={`register__input-error ${emailValid && 'register__input-error_active'}`}
+          >
+            {errors.email}
+          </span>
+        </label>
+        <button
+          type="submit"
+          className="profile__button profile__button_type_edit"
+        >
+          {titleSubmit}
+        </button>
+        <Link
+          to="/"
+          className="profile__button profile__button_type_logout"
+          onClick={onLogout}
+        >
+          {titleLogoutButton}
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 Profile.defaultProps = {
-  name: 'Друг',
   titleSubmit: 'Редактировать',
   titleLogoutButton: 'Выйти из аккаунта',
 };
 
 Profile.propTypes = {
-  name: PropTypes.string,
+  onSubmit: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
   titleSubmit: PropTypes.string,
   titleLogoutButton: PropTypes.string,
 };
