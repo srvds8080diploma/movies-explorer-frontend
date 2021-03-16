2fdb8afa15ad9bd9ae8e8f8db408e65713b0b937
srvds8080 <srvds@yandex.ru>
2021-03-16 18:35:21 +0300

    feat: Насторить базовую фенкциональность в App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import {
   Switch,
   Route,
@@ -24,29 +24,192 @@ import Login from '../Login/Login';
 import NotFound from '../NotFound/NotFound';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import ArrayLinks from '../../utils/constants';
+import MoviesApi from '../../utils/MoviesApi';
+import MainApi from '../../utils/MainApi';
+import SearchForm from '../SearchForm/SearchForm';
 
 const App = ({ location, history }) => {
   const [currentUserContext, setCurrentUserContext] = useState({});
+  const [width, setWith] = useState(window.innerWidth);
   const [valueButton, setValueButton] = useState(false);
   const [valueMenuBurger, setValueMenuBurger] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const handleClickButtonBurger = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isShort, setIsShort] = useState(false);
+  const [foundMovies, setFoundMovies] = useState([]);
+  const [savedMovies, setSavedMovies] = useState([]);
+  const handleCloseBurger = () => {
     setValueButton(!valueButton);
     setValueMenuBurger(!valueMenuBurger);
   };
-  const handleLogin = () => {
-    setIsLoggedIn(!isLoggedIn);
-    history.push('/movies');
+  const handleSetUserData = (data) => {
+    MainApi.setUserData(data)
+      .then((res) => setCurrentUserContext((prevState) => ({
+        ...prevState,
+        name: res.name,
+        email: res.email,
+      })))
+      .catch((err) => console.log(err));
   };
+  const handleLogout = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+    history.push('/');
+  };
+  const handleLogin = (data) => {
+    MainApi.authentication(data)
+      .then((res) => {
+        setCurrentUserContext(res);
+        localStorage.setItem('jwt', res.token);
+        setIsLoggedIn(true);
+        return history.push('/movies');
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleRegister = (data) => {
+    MainApi.registration(data)
+      .then(() => {
+        setIsLoggedIn(true);
+        return handleLogin({ email: data.email, password: data.password });
+      })
+      .catch(() => setIsLoggedIn(false));
+  };
+  const searchMovies = (movies, value) => {
+    const valueLowerCase = value.toLowerCase();
+    return new Promise(((resolve) => resolve(movies
+      .filter((item) => JSON
+        .stringify(item.nameRU)
+        .toLowerCase()
+        .includes(valueLowerCase)
+        || JSON
+          .stringify(item.nameEN)
+          .toLowerCase()
+          .includes(valueLowerCase)))));
+  };
+  const transformArray = (array) => array.map((card) => {
+    if (!card.image) {
+      console.log({ message: `неверные данные ${card.nameRU}` });
+      return {
+        movieId: 101,
+        nameRU: card.nameRU,
+        nameEN: card.nameEN,
+        country: card.country,
+        description: card.description,
+        director: card.director,
+        year: card.year,
+        duration: card.duration,
+        trailer: card.trailerLink,
+        thumbnail: 'https://api.nomoreparties.co/uploads/thumbnail_zagruzhennoe_53d33dc9df.jpeg',
+        image: 'https://api.nomoreparties.co/uploads/thumbnail_zagruzhennoe_53d33dc9df.jpeg',
+      };
+    }
+    return {
+      movieId: card.id,
+      nameRU: card.nameRU,
+      nameEN: card.nameEN,
+      country: card.country,
+      description: card.description,
+      director: card.director,
+      year: card.year,
+      duration: card.duration,
+      trailer: card.trailerLink,
+      thumbnail: `https://api.nomoreparties.co${card.image.formats.thumbnail.url}`,
+      image: `https://api.nomoreparties.co${card.image.url}`,
+    };
+  });
+  const handleCheckShort = (value) => {
+    setIsShort(value);
+  };
+  const handleSearch = (value) => {
+    let movies;
+    if (location.pathname === '/movies') {
+      movies = JSON.parse(localStorage.getItem('movies'));
+    } else if (location.pathname === '/saved-movies') {
+      movies = savedMovies;
+    }
+    if (!movies) {
+      MoviesApi.getData()
+        .then((newMovies) => localStorage.setItem('movies', JSON.stringify(newMovies)))
+        .catch((err) => console.log(err));
+      return;
+    }
+    if (value) {
+      searchMovies(movies, value)
+        .then((res) => {
+          if (res) {
+            return ((location.pathname === '/movies')
+              ? setFoundMovies(transformArray(res))
+              : setSavedMovies(transformArray(res)));
+          }
+          return console.log({ message: 'ничего не найдено' });
+        })
+        .catch((err) => console.log(err));
+    } else if (!value) {
+      console.log({ message: 'запрос не может быть пустым' });
+      setFoundMovies(transformArray(movies));
+    }
+  };
+  const handleLike = (card, isLiked) => {
+    console.log(card.movieId);
+    if (isLiked) {
+      return MainApi.deleteCard((savedMovies.find((item) => item.movieId === card.movieId))._id)
+        .then(() => {
+          const newSavedMovies = savedMovies.filter((item) => card._id !== item._id);
+          const newFoundMovies = foundMovies.filter((item) => card.movieId !== item.movieId);
+          setSavedMovies(newSavedMovies);
+          return setFoundMovies(newFoundMovies);
+        })
+        .catch((err) => console.log(err));
+    }
+    return MainApi.addCard(card)
+      .then(({ movie }) => (
+        setSavedMovies((prevState) => [movie, ...prevState])
+      ))
+      .catch((err) => console.log(err));
+  };
+  const resizeWindow = () => {
+    setTimeout(() => {
+      setWith((prevState) => ({
+        ...prevState,
+        width: window.innerWidth,
+      }));
+    }, 1000);
+  };
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      MainApi.tokenCheck(token)
+        .then((res) => {
+          setCurrentUserContext(res);
+          setIsLoggedIn(true);
+          return history.push('/movies');
+        })
+        .catch(() => {
+          setIsLoggedIn(false);
+          history.push('/');
+        });
+    }
+  }, []);
+  useEffect(() => {
+    MainApi.getCards()
+      .then((res) => setSavedMovies(res))
+      .catch((err) => console.log(err));
+  }, []);
+  useLayoutEffect(() => {
+    setIsShort(false);
+    window.addEventListener('resize', resizeWindow);
+    resizeWindow();
+    return () => window.removeEventListener('resize', resizeWindow);
+  }, []);
 
   return (
     <UserContext.Provider value={currentUserContext}>
       <MenuBurger
         classMenuBurgerValue={valueMenuBurger}
+        handleClick={handleCloseBurger}
       />
       <div className="page">
         <Header location={location}>
-          {isLoggedIn
+          {!isLoggedIn
             ? <MenuProfile />
             : (
               <>
@@ -55,7 +218,7 @@ const App = ({ location, history }) => {
                   <Account />
                 </Navigation>
                 <BurgerButton
-                  handleClickButtonBurger={handleClickButtonBurger}
+                  handleClickButtonBurger={handleCloseBurger}
                   classBurgerValue={valueButton}
                 />
               </>
@@ -69,8 +232,13 @@ const App = ({ location, history }) => {
             arrayFilms={foundMovies}
             savedMovies={savedMovies}
             onLike={handleLike}
+            width={width}
+            isShort={isShort}
           >
-            <SearchForm onSubmit={handleSearch} />
+            <SearchForm
+              onCheck={handleCheckShort}
+              onSubmit={handleSearch}
+            />
           </ProtectedRoute>
           <ProtectedRoute
             path="/profile"
@@ -87,8 +255,13 @@ const App = ({ location, history }) => {
             loggedIn={isLoggedIn}
             arrayFilms={savedMovies}
             onLike={handleLike}
+            width={width}
+            isShort={isShort}
           >
-            <SearchForm onSubmit={handleSearch} />
+            <SearchForm
+              onCheck={handleCheckShort}
+              onSubmit={handleSearch}
+            />
           </ProtectedRoute>
           <Route exact path="/">
             <Main />
